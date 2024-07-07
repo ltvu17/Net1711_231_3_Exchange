48d280a6db554c1e3fba9be54a0e1c849fb348b1
KuongGB <cuong3right@gmail.com>
2024-07-07 15:45:43 +0700

    fix product

--- ExchangeWebApp/Controllers/ProductController.cs
+++ ExchangeWebApp/Controllers/ProductController.cs
@@ -148,6 +148,7 @@ namespace ExchangeWebApp.Controllers
                     {
                         if (response.IsSuccessStatusCode)
                         {
+                            
                         }
                     }
                 }
